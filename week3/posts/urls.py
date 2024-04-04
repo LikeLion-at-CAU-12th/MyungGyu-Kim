dd480184e9bcb6e46adbe99b6f305721a8fa834f
Haemulramen <apple11078@gmail.com>
2024-04-04 09:58:16 +0900

    feat: CRUD 세션 실습

--- week3/posts/urls.py
+++ week3/posts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', student_view, name = 'student_view'),
-    path('introduction', introduction, name = 'introduction'),
-    path('<int:id>', get_post_detail, name='게시글 조회'),
+    # path('', hello_world, name = 'hello_world'),
+    # path('page', student_view, name = 'student_view'),
+    # path('introduction', introduction, name = 'introduction'),
+    # path('<int:id>', get_post_detail, name='게시글 조회'),
+    path('', post_list, name = 'post_list'),
+    path('<int:id>', post_detail, name = 'post_detail'),
 ]
--- week3/posts/views.py
+++ week3/posts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from .models import Student
 from posts.models import *
+import json
 
 def hello_world(request) :
     if request.method == "GET":
@@ -58,3 +59,113 @@ def get_post_detail(request, id):
         'message' : '게시글 조회 성공', 
         'data' : post_detail_json
     })
+
+@require_http_methods(["POST", "GET"])
+def post_list(request):
+
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+
+        new_post = Post.objects.create(
+            writer = body['writer'],
+            title = body['title'],
+            content = body['content'],
+            category = body['category']
+        )
+
+        new_post_json = {
+            "id" : new_post.id,
+            "writer" : new_post.writer,
+            "title" : new_post.title,
+            "content" : new_post.content,
+            "category" : new_post.category
+        }
+
+        # response = Post.objects.filter(id=4).delete()
+        # # response= new_post.delete()
+        # return JsonResponse({
+        #     "entity": response
+        # })
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 생성 성공",
+            "data" : new_post_json
+        })
+    
+    if request.method == "GET":
+        post_all = Post.objects.all()
+
+        post_json_all = []
+
+        for post in post_all:
+            post_json = {
+                "id" : post.id,
+                "title" : post.title,
+                "writer" : post.writer,
+                "content" : post.content,
+                "category" : post.category
+            }
+            
+            post_json_all.append(post_json)
+        
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 목록 조회 성공",
+            "data" : post_json_all
+        })
+    
+@require_http_methods(["GET", "PATCH", "DELETE"])
+def post_detail(request, id):
+    
+    if request.method == "GET":
+        post = get_object_or_404(Post, id=id)
+
+        post_json = {
+            "id" : post.id,
+            "title" : post.title,
+            "writer" : post.writer,
+            "content" : post.content,
+            "category" : post.category
+        }
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 조회 성공",
+            "data" : post_json
+        })
+    
+    if request.method == "PATCH":
+        body = json.loads(request.body.decode('utf-8'))
+        
+        update_post = get_object_or_404(Post, pk=id)
+
+        update_post.title = body["title"]
+        update_post.content = body["content"]
+        update_post.category = body["category"]
+
+        update_post.save()
+
+        update_post_json = {
+            "id" : update_post.id,
+            "title" : update_post.title,
+            "writer" : update_post.writer,
+            "content" : update_post.content,
+            "category" : update_post.category
+        }
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시물 수정 성공",
+            "data" : update_post_json
+        })
+
+    if request.method == "DELETE":
+        delete_post = get_object_or_404(Post, pk=id) 
+
+        delete_post.delete()
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 삭제 성공",
+            "data" : None
+        })
