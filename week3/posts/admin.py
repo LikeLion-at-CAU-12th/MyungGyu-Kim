09f4d01992c20127a86087d75e629b94cbce21aa
Haemulramen <apple11078@gmail.com>
2024-04-06 15:31:50 +0900

    FEAT: Image 모델 등록

--- week3/posts/admin.py
+++ week3/posts/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Hashtag)
+admin.site.register(Image)
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['post_id','image_view']
+    
+    def image_view(self, image):
+        if image.image:
+            return mark_safe(f'<img src="{image.image.url}" style="width:50px;" />')
+        return None
