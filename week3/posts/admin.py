7a6cfd148dd2ed68573ac89f8f046ffa8cc7f450
Haemulramen <apple11078@gmail.com>
2024-04-07 10:08:08 +0900

    FEAT: admin 페이지에서 이미지 파일 볼 수 있도록 수정

--- week3/posts/admin.py
+++ week3/posts/admin.py
@@ -7,12 +7,13 @@ admin.site.register(Student)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Hashtag)
-admin.site.register(Image)
 
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['post_id','image_view']
+    list_display = ['post_id','photo_tag']
     
-    def image_view(self, image):
+    def photo_tag(self, image):
         if image.image:
             return mark_safe(f'<img src="{image.image.url}" style="width:50px;" />')
         return None
+    photo_tag.short_description = 'Image'
